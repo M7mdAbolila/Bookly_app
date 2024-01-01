3dcc54a0020e485c1e9d560c8ae76ff2a0a074a7
M7mdAbolila <m7md.abolila.cs1@gmail.com>
2024-01-01 13:25:58 +0200

    add custom scrol view to details view

--- lib/features/home/presentation/views/widgets/book_betails_body.dart
+++ lib/features/home/presentation/views/widgets/book_betails_body.dart
@@ -13,69 +13,78 @@ class BookDetailsBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const BookDetailsAppBar(),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * .2),
-            child: const BookItem(),
-          ),
-          const SizedBox(
-            height: 46,
-          ),
-          SizedBox(
-            width: MediaQuery.of(context).size.width * .5,
-            child: Text(
-              'Harry Potter and the Goblet of Fire ',
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-              style: GoogleFonts.lora(textStyle: Styles.textStyle20),
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const BookDetailsAppBar(),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: width * .2),
+                  child: const BookItem(),
+                ),
+                const SizedBox(
+                  height: 46,
+                ),
+                SizedBox(
+                  width: width * .5,
+                  child: Text(
+                    'Harry Potter and the Goblet of Fire ',
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: GoogleFonts.lora(textStyle: Styles.textStyle20),
+                  ),
+                ),
+                const SizedBox(
+                  height: 6,
+                ),
+                Opacity(
+                  opacity: .7,
+                  child: Text(
+                    'J.K. Rowling',
+                    style: Styles.textStyle18.copyWith(
+                      fontStyle: FontStyle.italic,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 18,
+                ),
+                const BookRating(),
+                const SizedBox(
+                  height: 18,
+                ),
+                const BooksAction(),
+                const Expanded(
+                  child: SizedBox(
+                    height: 40,
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'You can also like',
+                    style: Styles.textStyle14.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                const SimilarBooksListView(),
+                const SizedBox(
+                  height: 20,
+                ),
+              ],
             ),
           ),
-          const SizedBox(
-            height: 6,
-          ),
-          Opacity(
-            opacity: .7,
-            child: Text(
-              'J.K. Rowling',
-              style: Styles.textStyle18.copyWith(
-                fontStyle: FontStyle.italic,
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 18,
-          ),
-          const BookRating(),
-          const SizedBox(
-            height: 18,
-          ),
-          const BooksAction(),
-          const SizedBox(
-            height: 40,
-          ),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              'You can also like',
-              style: Styles.textStyle14.copyWith(
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          const SimilarBooksListView(),
-          const SizedBox(
-            height: 20,
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }

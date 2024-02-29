554b29fe31daf2c09827319a93f02788e59080a2
M7mdAbolila <m7md.abolila.cs1@gmail.com>
2024-02-29 12:09:06 +0200

    setup Service locator

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -8,7 +8,7 @@ import 'package:dio/dio.dart';
 class HomeRepoImpl implements HomeRepo {
   final ApiService apiService;
 
-  HomeRepoImpl({required this.apiService});
+  HomeRepoImpl( this.apiService);
   @override
   Future<Either<Failure, List<BookModel>>> fetechNewsetBooks() async {
     try {
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
+import 'package:bookly_app/core/utils/service_locator.dart';
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  setupServiceLocator();
   runApp(const BooklyApp());
 }
 
